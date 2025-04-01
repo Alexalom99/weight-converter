64a871b4fa6a827576aa9945d3f51d242a588f90
Alexalom99 <alexalom1999@gmail.com>
2025-04-01 20:46:32 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Buena suerte
+const input = document.querySelector("#input");
+const result = document.querySelector("#result");
+
+input.addEventListener("input", function() {
+    result.textContent = parseFloat(input.value*0.453592).toFixed(2);
+})
